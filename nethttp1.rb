0afe07d740f90ddab092ee8325f6ddb53cace2d2
José Carlos García <jcarlosgarcia@hotmail.com>
2012-02-05 16:17:10 +0100

    Net::HTTP.start

---
+++ nethttp1.rb
@@ -0,0 +1,8 @@
+require 'net/http'
+
+url = URI.parse('http://rubylearning.com/data/text.txt')
+
+Net::HTTP.start(url.host, url.port) do |http|
+  req = Net::HTTP::Get.new(url.path)
+  puts http.request(req).body
+end
