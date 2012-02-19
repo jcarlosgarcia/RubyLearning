264fb667db13764bb926b390364ea3e16a74279d
José Carlos García <jcarlosgarcia@hotmail.com>
2012-02-19 16:53:40 +0100

    Rack::Request and Rack::Response example

---
+++ rack_request/my_request.rb
@@ -0,0 +1,14 @@
+class MyRequest
+  def call(env)
+    req = Rack::Request.new(env)
+    name = req.params['name']
+    text = req.params['text']
+
+    Rack::Response.new.finish do |res|
+      res['Content-Type'] = 'text/plain'
+      res.status = 200
+      str = "Parameters sent: name - #{name} | text - #{text}"
+      res.write str
+    end
+  end
+end
