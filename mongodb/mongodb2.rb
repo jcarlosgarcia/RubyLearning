59a8cd610c95627c74ab80316832ab8607c9e9a0
José Carlos García <jcarlosgarcia@hotmail.com>
2012-03-03 13:43:00 +0100

    Using MongoHQ

---
+++ mongodb/mongodb2.rb
@@ -0,0 +1,18 @@
+require 'mongo'
+
+db = Mongo::Connection.new('flame.mongohq.com', 27107).db("contacts")
+auth = db.authenticate(ARGV[0], ARGV[1])
+
+coll = db.collection("contacts")
+
+# Inserting a document
+docs = [{:name => 'Manisha', :loves => ['english', 'marathi'], :gender => 'f', :country => 'india'},
+	{:name => 'Satoshi', :loves => ['ruby','c'], :gender => 'm', :country => 'japan'},
+	{:name => 'Michael', :loves => ['ruby','clojure'], :gender => 'm', :country => 'austria'}]
+
+docs.each { |doc| coll.insert(doc) }
+
+puts "#{coll.count} docs inserted"
+
+coll.find.each { |doc| puts doc.inspect}
+
