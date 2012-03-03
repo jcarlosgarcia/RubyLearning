1c51abb9701aa9420d25a9fad1b2a21ff3607091
José Carlos García <jcarlosgarcia@hotmail.com>
2012-03-03 13:46:05 +0100

    Comment removed

--- mongodb/mongodb2.rb
+++ mongodb/mongodb2.rb
@@ -5,7 +5,6 @@ auth = db.authenticate(ARGV[0], ARGV[1])
 
 coll = db.collection("contacts")
 
-# Inserting a document
 docs = [{:name => 'Manisha', :loves => ['english', 'marathi'], :gender => 'f', :country => 'india'},
 	{:name => 'Satoshi', :loves => ['ruby','c'], :gender => 'm', :country => 'japan'},
 	{:name => 'Michael', :loves => ['ruby','clojure'], :gender => 'm', :country => 'austria'}]
